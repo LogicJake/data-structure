61c03928c03049e3494076ead3b20064a2c6ad17
LogicJake <835410808@qq.com>
2016-12-02 22:11:34 +0800

    Update ALGraph
    add 求关键路径

--- ALGraph.cpp
+++ ALGraph.cpp
@@ -1,6 +1,7 @@
-//有向表 邻接表 
+//邻接表 
 #include <iostream>
 #include <stack> 
+#include <string.h>
 using namespace std;
 #define OK 1
 #define ERROR 0
@@ -14,6 +15,7 @@ typedef int Status;
 typedef struct ArcNode{
 	int adjvex;
 	struct ArcNode *nextarc;
+	int info;		//存储弧长； 
 }ArcNode;
 typedef struct VNode{
 	VertexType data;
@@ -33,9 +35,12 @@ int LocateVex(ALGraph G, VertexType v1)
 }
 Status CreateGraph(ALGraph &G)
 {
-	int i,j,k;
+	int i,j,k,value;
+	char choice;
 	VertexType v1,v2;
 	ArcNode *s;
+	cout<<"是否有权？(y or n)";
+	cin>>choice;
 	cin>>G.vexnum>>G.arcnum;
 	for(i = 0; i < G.vexnum; i++)
 	{
@@ -44,10 +49,14 @@ Status CreateGraph(ALGraph &G)
 	}
 	for(k = 0; k < G.arcnum; k++)
 	{
-		cin>>v1>>v2;
+		if(choice == 'y')	//有权 
+			cin>>v1>>v2>>value;
+		else
+			cin>>v1>>v2;
 		i = LocateVex(G,v1);
 		j = LocateVex(G,v2);
 		s = new ArcNode;
+		s->info = value;
 		s->adjvex = j;
 		s->nextarc = G.vertices[i].firstarc;
 		G.vertices[i].firstarc = s;
@@ -67,6 +76,8 @@ void PRINT(ALGraph G)
         printf("\n");
     }
 }
+
+/**************拓扑排序***********************/ 
 void FindInDegree(ALGraph G,int *indegree)
 {
 	int i;
@@ -112,18 +123,109 @@ Status TopologicalSort(ALGraph &G)		//如果可以拓扑排序 说明不存在环
 	else
 		return OK;
 } 
+
+/******************改造拓扑排序求关键路径**********************************/
+stack<int> T;	//T为拓扑序列顶点栈，s为0入度顶点栈 
+int ve[MAX_VERTEX_NUM]; 	//全局变量，各顶点事件的最早发生时间 
+Status TopologicalOrder(ALGraph &G, stack<int> &T)
+{
+	int indegree[G.vexnum];
+	int i,k;
+	ArcNode *p; 
+	FindInDegree(G,indegree);
+	memset(ve,0,G.vexnum*sizeof(int));
+ 	stack<int> s;
+ 	for(i = 0; i < G.vexnum; i++)
+ 		if(!indegree[i])
+ 			s.push(i);
+ 	int count = 0;
+ 	while(!s.empty())
+ 	{
+ 		i = s.top();
+ 		s.pop();
+ 		T.push(i); 
+ 		count++;
+ 		for(p = G.vertices[i].firstarc; p; p = p->nextarc)
+ 		{
+ 			k = p->adjvex;
+			if(!(--indegree[k]))
+				s.push(k);	
+			if(ve[i]+(p->info) > ve[k])
+				ve[k] = ve[i]+(p->info);
+		 }		
+	 }
+	if(count < G.vexnum)
+		return ERROR;
+	else
+		return OK;
+}
+Status CriticalPath(ALGraph G)
+{
+	int i,j,k,dut,ee,el;
+	char tag;
+	ArcNode *p;
+	if(!TopologicalOrder(G,T))
+		return ERROR;
+	int vl[G.vexnum];
+	for(i = 0; i < G.vexnum-1; i++)
+		vl[i] = INT_MAX;
+	vl[G.vexnum-1] = ve[G.vexnum-1];
+	while(!T.empty())	//按拓扑逆排序求各顶点vl值 
+	{
+		for(j = T.top(),T.pop(),p = G.vertices[j].firstarc; p; p = p->nextarc) 
+		{
+			k = p->adjvex;
+			dut = p->info;
+			if(vl[k] - dut < vl[j])
+				vl[j] = vl[k] - dut;
+		}
+	}
+	cout<<"关键路径："<<endl; 
+	for(j = 0; j < G.vexnum; j++)
+	{
+		for(p = G.vertices[j].firstarc; p; p = p->nextarc)
+		{
+			k = p->adjvex;
+			dut = p->info;
+			ee = ve[j];
+			el = vl[k] - dut;
+			tag = (ee == el)?'*':' ';
+			if(tag == '*')
+				cout<<G.vertices[j].data<<" "<<G.vertices[k].data<<" "<<dut<<" "<<ee<<" "<<el<<" "<<endl; 
+		}
+	}
+}
 int main()
 {
 	ALGraph G;
 	CreateGraph(G);
 	PRINT(G);
-	TopologicalSort(G);
+//	TopologicalSort(G);
+	CriticalPath(G);
  } 
- /*
-4 4
-1 2 3 4
-1 2
-1 3
-3 4
-4 1
+/*测试数据 
+9 11
+1 2 3 4 5 6 7 8 9
+1 2 6
+1 3 4
+1 4 5
+2 5 1
+3 5 1
+4 6 2
+5 7 9
+5 8 7
+6 8 4
+7 9 2
+8 9 4
+
+6 8
+1 2 3 4 5 6
+1 2 3
+1 3 2
+2 4 2
+2 5 3
+3 4 4
+3 6 3
+4 6 2
+5 6 1
  */
