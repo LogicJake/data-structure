b03694ac29a70a9897e77343e857b006130c62ad
LogicJake <835410808@qq.com>
2016-12-20 18:19:45 +0800

    Update Graph.h
    add"求最短路径"

--- Graph.h
+++ Graph.h
@@ -6,7 +6,7 @@ using namespace std;
 #define ERROR 0
 #define TRUE 1
 #define FALSE 0
-#define INFINITY INT_MAX
+#define INFINITY 1000000000
 #define MAX_VERTEX_NUM 20
 typedef int VertexType;
 typedef int VRType;
@@ -48,7 +48,7 @@ Status CreateGraph(MGraph &G)
 		i = LocateVex(G,v1);
 		j = LocateVex(G,v2);
 		G.arcs[i][j].adj = value;
-		G.arcs[j][i].adj = G.arcs[i][j].adj;
+	//	G.arcs[j][i].adj = G.arcs[i][j].adj;
 	}
 	return OK;
 }
@@ -243,13 +243,63 @@ void MiniSpanTree_PRIM(MGraph G, VertexType u)
 			}
 	}
 }
-//普里姆算法end
-
+/*********************最短路径************************/
+void ShortPath(MGraph G, int v0, int **P, int *D)
+{
+	int i,v,w,min;
+	int final[MAX_VERTEX_NUM];
+	for(v = 0; v < G.vexnum; v++)
+	{
+		final[v] = FALSE;
+		D[v] = G.arcs[v0][v].adj;
+		for(w = 0; w < G.vexnum; w++)
+			P[v][w] = FALSE;	//空路径 
+		if(D[v] < INFINITY){
+			P[v][v0] = TRUE;
+			P[v][v] = TRUE;
+		} 
+	}
+	D[v0] = 0;
+	final[v0] = TRUE;
+	for(i = 1; i < G.vexnum; i++)
+	{
+		min = INFINITY;
+		for(w = 0; w < G.vexnum; w++)
+		{
+			if(!final[w])
+				if(D[w] < min)
+				{
+					v = w;
+					min = D[v];
+				}
+		}
+		final[v] = TRUE;
+		for(w = 0; w < G.vexnum; w++)
+		{
+			if(!final[w]&&(min + G.arcs[v][w].adj < D[w])){
+				D[w] = min + G.arcs[v][w].adj;
+				for(int i = 0; i < G.vexnum; i++)
+					P[w][i] = P[v][i];	
+				P[w][w] = TRUE;	
+			}
+		}
+	}
+ } 
 int main()
 {
-	MGraph G;
-	CreateGraph(G);
-	PRINT(G);
+//	MGraph G;
+//	CreateGraph(G);
+//	int **p;
+//	p = new int *[MAX_VERTEX_NUM];
+//	for(int i = 0; i < MAX_VERTEX_NUM; i ++)
+//    	p[i] = new int[MAX_VERTEX_NUM];
+//	int d[MAX_VERTEX_NUM];
+//	ShortPath(G,0,p,d);
+//	for(int i = 0; i < G.vexnum; i ++){
+//		for(int j = 0; j < G.vexnum; j++)
+//			cout<<p[i][j];
+//		cout<<endl;
+	}
 //	DFSTraverse(G);
 //	cout<<endl;
 //	BFSTraverse(G);
@@ -271,11 +321,14 @@ int main()
 //	return 0;
  } 
 /*测试数据 
-6 5
-1 2 3 4 5 6 
-1 2 1
-2 3 1
-2 4 1
-4 5 1
-4 6 1
+6 8
+0 1 2 3 4 5 
+0 5 100
+0 4 30
+0 2 10
+1 2 5
+2 3 50
+3 5 10
+4 5 60
+4 3 20
 */
